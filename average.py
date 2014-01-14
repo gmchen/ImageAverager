75d045186c0ef0d75b54272ace9526952db1419f
Gregory M Chen <GregoryMChen@gmail.com>
2014-01-14 00:23:13 -0500

    Adjusted comments

--- average.py
+++ average.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2
 from os import listdir
 
-# Path to image directory
-image_dir = "/home/greg/Dropbox/Greg/Python/ImageAverager/faces/All/"
+image_dir = "/home/greg/Dropbox/Greg/Python/ImageAverager/faces/All/"  # Path to image directory
+
+# Load images
 filenames = listdir(image_dir)
 filenames.sort()
 
@@ -11,6 +12,7 @@ imgs = []
 for f in filenames:
 	imgs.append(cv2.imread(image_dir + f,0))
 
+# Find the dimensions to use in the final image
 max_height = 0
 max_width = 0
 for i in imgs:
@@ -19,6 +21,7 @@ for i in imgs:
 	if i.shape[1] > max_width:
 		max_width = i.shape[1]
 
+# Produce a list of centered images
 centered_imgs = []
 for i in imgs:
 	height, width = i.shape
@@ -29,10 +32,13 @@ for i in imgs:
 	new_img[start_height:(start_height + height), start_width:(start_width + width)] = i
 	centered_imgs.append(new_img)
 
+# Apply the weighted average
 final_img = np.zeros([max_height, max_width], np.uint8)
 for i in range(len(centered_imgs)):
+	# we can prove by straightforward induction that this will result in equal weighting for all images
 	final_img = cv2.addWeighted(final_img, i/(i+1.0), centered_imgs[i], 1/(i+1.0), 0)
 
+# Display the final image, allow for saving
 cv2.imshow('Final Image', final_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
